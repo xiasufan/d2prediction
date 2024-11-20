c86e5f188bc8b41465e37cab2271f81fd74e7aad
xiasufan <sufanx@student.unimelb.edu.au>
2024-11-21 08:18:59 +1100

    assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import DotaPrediction from './Components/main';
 
 function App() {
   return (
-    <div className="h-screen flex justify-center items-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Tailwind test</h1>
+    <div className="App">
+      <DotaPrediction />
     </div>
   );
 }
