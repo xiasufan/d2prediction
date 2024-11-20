5239882646226a012e928abc857fbe8e2b5526d7
xiasufan <sufanx@student.unimelb.edu.au>
2024-11-20 22:48:59 +1100

    Init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Tailwind test</h1>
+    </div>
+  );
+}
+
+export default App;
